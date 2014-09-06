3724302251f1ada43497b073defe15d069a88d5f
Daniela Kazarian <dbkazarian@gmail.com>
2014-09-06 16:58:54 -0300

    Probando put y post

--- grupo4/src/main/java/tacs/grupo4/resources/Items.java
+++ grupo4/src/main/java/tacs/grupo4/resources/Items.java
@@ -1,9 +1,15 @@
 package tacs.grupo4.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+
+import com.sun.jersey.core.header.MediaTypes;
 
 @Path("/items")
 public class Items {
@@ -15,14 +21,28 @@ public class Items {
     // The Java method will produce content identified by the MIME Media
     // type "text/plain"
     @Produces("text/plain")
-    public String getIt() {
+    public String index() {
         return "Listado items";
     }
     
     @GET
-    @Path("/{item_id}")
-    public String item(@PathParam("item_id") Integer id){
+    @Path("/{id}")
+    public String show(@PathParam("id") Integer id){
     	return "Item "+id.toString();
     }
     
+    @POST
+    @Produces("text/plain")
+	public String create(@FormParam("name") String nombre) {
+   	
+    	return "Item "+nombre+" created";
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces("text/plain")
+	public String update(@PathParam("id") Integer id, @FormParam("name") String name) {
+   	
+    	return "Item "+id+" name changed to: "+ name;
+    }
 }
