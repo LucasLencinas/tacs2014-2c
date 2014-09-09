637290ac5b6c8bb7bf9d7cb06804d06badb96387
Daniela Kazarian <dbkazarian@gmail.com>
2014-09-09 15:10:30 -0300

    Mejoro respuestas de abm de items

--- grupo4/src/main/java/tacs/grupo4/resources/Items.java
+++ grupo4/src/main/java/tacs/grupo4/resources/Items.java
@@ -1,6 +1,6 @@
 package tacs.grupo4.resources;
 
-import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,41 +8,51 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
-import com.sun.jersey.core.header.MediaTypes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/items")
 public class Items {
 
-    // TODO: update the class to suit your needs
-    
-    // The Java method will process HTTP GET requests
     @GET 
-    // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
     @Produces("text/plain")
     public String index() {
         return "Listado items";
     }
     
+      
     @GET
     @Path("/{id}")
-    public String show(@PathParam("id") Integer id){
-    	return "Item "+id.toString();
+    public Response show(@PathParam("id") Integer id){
+    	String json = "{id:"+id+ ",title:'example',description:'example',mercadolibre:{permalink: 'http://articulo.mercadolibre.com.ar/MLA-430387888-anteojos-ray-ban-wayfare-_JM',id:'MLA430387888'}}";
+    	return Response.ok(json, MediaType.APPLICATION_JSON).build();
     }
     
     @POST
-    @Produces("text/plain")
-	public String create(@FormParam("name") String nombre) {
-   	
-    	return "Item "+nombre+" created";
+	public Response create(
+			@FormParam("title") String title,
+			@FormParam("description") String description, 
+			@FormParam("mercadolibre[permalink]") String permalink, 
+			@FormParam("mercadolibre[id]") String ma_id) {
+    	String json = "{id: 2, title:'"+title+"',description:'"+description+"',mercadolibre:{permalink: '"+permalink+",id:'"+ma_id+"'}}";
+    	return Response.ok(json, MediaType.APPLICATION_JSON).build();
     }
 
     @PUT
     @Path("/{id}")
-    @Produces("text/plain")
-	public String update(@PathParam("id") Integer id, @FormParam("name") String name) {
-   	
-    	return "Item "+id+" name changed to: "+ name;
+	public Response update(
+			@PathParam("id") Integer id, 			
+			@FormParam("title") String title,
+			@FormParam("description") String description, 
+			@FormParam("mercadolibre[permalink]") String permalink, 
+			@FormParam("mercadolibre[id]") String ma_id) {
+    	String json = "{id: "+id+", title:'"+title+"',description:'"+description+"',mercadolibre:{permalink: '"+permalink+",id:'"+ma_id+"'}}";
+    	return Response.ok(json, MediaType.APPLICATION_JSON).build();
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public Response destroy(@PathParam("id") Integer id){
+    	return Response.ok().build();
     }
 }
