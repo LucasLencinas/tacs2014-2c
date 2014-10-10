0b2f3881645c0cce9851df8e82db3343e49834ec
fangta1108 <fangta1108@gmail.com>
2014-10-10 00:23:11 -0300

    fix

--- src/main/resources/com/tacs/truequeLibre/client/js/index.js
+++ src/main/resources/com/tacs/truequeLibre/client/js/index.js
@@ -1,5 +1,5 @@
 
-	function getMyItems(){
+	function getMyTrueques(){
 		$.get("truequeLibre/trueques", function( data ) {
 		  $( "#mainTitle" ).html( "Mis Trueques" );
 		  $( "#mainData" ).load( "trueques.html" );
