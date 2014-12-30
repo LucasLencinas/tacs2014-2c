e861e08e61d9acdd5ce1c249486c2d6602d65f50
Daniela Kazarian <dbkazarian@gmail.com>
2014-12-29 22:58:43 -0300

    Ebay search
    frontend

--- src/main/webapp/js/new.js
+++ src/main/webapp/js/new.js
@@ -64,7 +64,90 @@ var MlSearch = function(table, template, after_results){
 		return item;
 	}
 
-	return this;
+}
+
+
+var EbaySearch = function(table, template, after_results){
+	this.template = template; 
+	this.table = $(table);
+	this.after_results = after_results;
+	var that = this;
+
+	/*
+		query: String a buscar
+		limit: Elementos por pagina
+		page: Página
+	*/
+
+	this.search = function(query, limit, page){
+		that.query = query;
+		$.ajax({
+		  type: "GET",
+		  dataType: "jsonp",
+		  url: "http://svcs.ebay.com/services/search/FindingService/v1",
+		  data: {
+		  	"OPERATION-NAME": "findItemsAdvanced",
+		  	"SERVICE-VERSION": "1.0.0",
+		  	"SECURITY-APPNAME": "DanielaK-07ea-4682-bcd6-1c4b2907b7ea",
+		  	"RESPONSE-DATA-FORMAT": "JSON",
+		  	"REST-PAYLOAD": "true",
+		  	"paginationInput.entriesPerPage": limit, 
+		  	"paginationInput.pageNumber": page,
+		  	"keywords": query,
+		  	"descriptionSearch": "true",
+		  	"GLOBAL-ID": that.language
+			},
+		  success: fill_table									
+		});
+	}
+
+
+	this.set_language = function(language){
+		that.language = language;
+	}
+
+
+	this.set_page = function(page){
+		page = Math.max(1, Math.min(that.query_result.paging.totalPages, page));
+		that.search(that.query_result.query, that.query_result.paging.limit, page);
+	}
+
+	this.next_page = function(){
+		that.set_page(that.query_result.paging.page + 1);
+	}
+	this.prev_page = function(){
+		that.set_page(that.query_result.paging.page - 1);
+	}
+
+
+	/*Private*/
+	function fill_table (data){
+		that.table.html("");
+		var pagination = data.findItemsAdvancedResponse[0].paginationOutput[0];
+		that.query_result = {"results": data.findItemsAdvancedResponse[0].searchResult[0].item, "paging": {"page": parseInt(pagination.pageNumber[0]), "limit": parseInt(pagination.entriesPerPage[0]), "totalPages": parseInt(pagination.totalPages[0]) }, "query": that.query} ;
+		if(typeof(that.query_result.results)!="undefined"){
+			$.each(that.query_result.results, function(i,r){
+				that.table.append(fill_item(r));
+			});
+		}
+		that.after_results();
+	}
+
+	function fill_item(item){
+		var item_html =  $(that.template
+						.replace(/%TITLE%/g, item.title[0])
+						.replace(/%THUMBNAIL%/g, item.galleryURL[0])
+						.replace(/%PERMALINK%/g, item.viewItemURL[0]));
+		item_html.data(
+				{	
+					"id": item.itemId[0],
+					"permalink": item.viewItemURL[0], 
+					"thumbnail": item.galleryURL[0],
+					"title": item.title[0],
+				}
+			);
+		return item_html;
+	}
 
 }
 
@@ -83,34 +166,64 @@ var MlSearch = function(table, template, after_results){
 
 
 
-/* Crea una instancia de MLSearch y setea los handlers a los botones */
 $(document).ready(function(){
+
+/* Crea una instancia de MLSearch y setea los handlers a los botones */
 	
 	var ml = new MlSearch("#ml-results", $("#ml-item-template").html(), showNavigationLinks);
+	var ebay = new EbaySearch("#ml-results", $("#ml-item-template").html(), showNavigationLinks);
+	
 	$('#title').on('keyup', function(e) {
 	    if (e.which == 13) {
 	        e.preventDefault();
 	        $("#ml-search-btn").click();
 	    }
 	});
+
 	function showNavigationLinks(){
 		$(".page-navigator").removeClass("hidden");
 	}
+
+	function hideNavigationLinks(){
+		$(".page-navigator").addClass("hidden");
+	}
+
+
+	function getSearchObjByProvider(){
+		var provider = $("#provider").val();
+		if(provider=="mercadolibre") return ml;
+		if(provider=="ebay"){
+			if($("#provider :selected").text().indexOf("US")>-1){
+				ebay.set_language("EBAY-US");	
+			}else{
+				ebay.set_language("EBAY-ES");
+			}
+
+			return ebay;	
+		} 
+	}
+
+
+	$("#provider").change(function(e){
+		$("#ml-results").html("");
+		hideNavigationLinks();
+	});
+
 	$("#ml-search-btn").click(
 		function(e){
 			e.preventDefault();
-			ml.search( $("#title").val(), 3);
+			getSearchObjByProvider().search( $("#title").val(), 3);
 		}
 	);
 
 	$("#ml-prev-page").click(function(e){
 		e.preventDefault();
-		ml.prev_page();
+		getSearchObjByProvider().prev_page();
 	});
 
 	$("#ml-next-page").click(function(e){
 		e.preventDefault();
-		ml.next_page();
+		getSearchObjByProvider().next_page();
 	});
 
 	/* Guarda en campos del form el permalink y la id del item seleccionado */
@@ -122,6 +235,7 @@ $(document).ready(function(){
 		$("#ml_thumbnail").val(ml_item.data().thumbnail);
 
 	});
+
 
 	$("#create-item").click(function(e){
 			var item = {
